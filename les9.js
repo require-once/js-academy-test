917f0cc51ae1e94d0bf2bb81718d7434bb3c6121
require-once <dimas.zhukov@gmail.com>
2020-09-24 13:15:44 +0300

    lesson 9

---
+++ les9.js
@@ -0,0 +1,51 @@
+"use strict";
+
+/* WAT */
+
+/*
+console.log([] + []);
+console.log([] + {});
+{
+	console.log({} + []);		// без console.log(). т.к. с ним выдаст то же, что и во 2 примере 
+}
+console.log({} + {});			// без console.log() в Firefox и в Chrome - разное поведение
+
+console.log(Array(3).join('wat' - 1));
+*/
+
+
+// console.log([1, 2, 3].map(parseInt));
+
+
+/*
+var a = 1
+var b = 1
+a
+++
+b
+console.log(a, b)
+*/
+
+
+/*
+;;;;;;;;;;;;;;;
+'Я комментирую свой код вот таким образом ;)'
+;;;;;;;;;;;;;;;
+*/
+
+
+/*
+var isPositiveNumber = function(num) {
+	return num > 0;
+};
+
+console.log(isPositiveNumber(null));
+*/
+
+
+(function() {
+	console.log(a);
+	var a = 1;
+	console.log(a);
+})();
+
